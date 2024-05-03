98d78929ed1163fdfe48c75f4a619ce7674639ff
myeongin <myeongin@jellybus.com>
2024-05-03 11:45:47 +0900

    이미지 카테고리, 스타일 카테고리 분기처리

--- Main.java
+++ Main.java
@@ -8,38 +8,43 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+    public static String[] words;
 
     public static void main(String[] args) {
         int type = 0;
         String filePath = "data.txt";
-        System.out.println("1.category");
+        System.out.println("1.image catagory 2.style category");
 
         try (Scanner sc = new Scanner(System.in)) {
             type = sc.nextInt();
         }
 
-        if (type == 1) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
 
-            try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-                String line;
+            while((line = br.readLine()) != null) {
+                String imageData = parsing(line, type);
+                System.out.println(imageData);
+            }
 
-                while((line = br.readLine()) != null) {
-                    String imageData = parsing(line);
-                    System.out.println(imageData);
-                }
-
-            } catch (IOException e) {
-                System.out.print(e.getMessage());
-            }   
-        }
-
+        } catch (IOException e) {
+            System.out.print(e.getMessage());
+        }   
 
     }
 
-    public static String parsing(String str) {
-        String[] words = str.split(":");
-        
-        return words[0];
+    public static String parsing(String str, int type) {
+        switch (type) {
+            case 1:         
+                words = str.split(":");       
+                return words[0];
+            case 2:
+                words = str.split(":"); 
+                return words[1];
+            default:
+                return "";
+        }
+
     }
 
 }
